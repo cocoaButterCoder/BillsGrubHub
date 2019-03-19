a954b10c4971f83f994812bc2ecc237b61ff1639
Bryan <cocoabuttercoder@gmail.com>
2019-03-18 22:50:09 -0400

    Created a Main Menu

---
+++ app/src/main/java/com/cocoabuttercoder/billsgrubhub/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package com.cocoabuttercoder.billsgrubhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
